e848e6f72bf9af64cb355efff7785169fe2838f8
Anurag Srivastava <anurag.131092@gmail.com>
2021-04-08 22:16:22 +0530

    Added axes and grid helpers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
 import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { extend, Canvas, useFrame, useThree } from '@react-three/fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'three'
 
-function Box(props) {
+// Make OrbitControls known as <orbitControls />
+extend({ OrbitControls })
+
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls component.
+  const {
+    camera,
+    gl: { domElement },
+    scene
+  } = useThree()
+  , inspect = () => {
+    window.camera = camera
+    window.THREE = THREE
+    window.scene = scene
+  }
+  , setControlParams = () => {
+    controls.current.minPolarAngle = Math.PI / 4 + .3;
+    controls.current.maxPolarAngle = Math.PI / 4 + .5;
+
+    // For Orthographic camera
+    controls.current.minZoom = 12;
+    controls.current.maxZoom = 24;
+
+    controls.current.enableDamping = true;
+    controls.current.dampingFactor = 0.05;
+    controls.current.autoRotate = true;
+    controls.current.autoRotateSpeed = .2;
+    controls.current.enablePan = false;
+    controls.current.enableKeys = false;
+  }
+
+  // inspect()
+
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef()
+  useFrame(() => { controls.current.update()})
+
+  // If we need to set parameters for controls
+  // controls.current && setControlParams()
+
+  return <orbitControls ref={controls} args={[camera, domElement]} />
+}
+, Box = props => {
   // This reference will give us direct access to the mesh
   const mesh = useRef()
   // Set up state for the hovered and active state
@@ -31,6 +76,9 @@ export default function App() {
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
+      <gridHelper args={[1000, 100]}/>
+      <axesHelper args={[500]} /> 
+      <CameraControls />
       <Box position={[0, 0, 0]} />
     </Canvas>
   )
